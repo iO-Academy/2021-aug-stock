f05f03579f4f87d1cbb5de8458dc7137ca6382cf
HannaSoderlund <hanna.ej.soderlund@gmail.com>
2021-12-13 17:03:53 +0000

    added ProjectController and ProductService files

--- Controllers/ProductController.js
+++ Controllers/ProductController.js
@@ -2,3 +2,8 @@ const dbConnection = require("../Services/DbService")
 const JsonResService = require("../Services/JsonResService")
 const ProductService = require("../Services/ProductService")
 
+let ProductController = {
+
+}
+
+module.exports = ProductController
