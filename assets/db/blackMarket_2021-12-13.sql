addc38ea533fe097b15afff0c4ab98a5081fda63
Reductito <damianbell96@gmail.com>
2021-12-13 16:35:03 +0000

    added sql db file to repo in /assets/db

---
+++ assets/db/blackMarket_2021-12-13.sql
@@ -0,0 +1,59 @@
+# ************************************************************
+# Sequel Pro SQL dump
+# Version 4541
+#
+# http://www.sequelpro.com/
+# https://github.com/sequelpro/sequelpro
+#
+# Host: 127.0.0.1 (MySQL 5.7.35)
+# Database: blackMarket
+# Generation Time: 2021-12-13 16:34:25 +0000
+# ************************************************************
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+
+# Dump of table products
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `products`;
+
+CREATE TABLE `products` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `product-name` varchar(50) NOT NULL DEFAULT '',
+  `price` decimal(13,2) unsigned NOT NULL DEFAULT '0.00',
+  `stock-quantity` smallint(5) unsigned NOT NULL DEFAULT '0',
+  `sku` varchar(10) NOT NULL DEFAULT '',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+LOCK TABLES `products` WRITE;
+/*!40000 ALTER TABLE `products` DISABLE KEYS */;
+
+INSERT INTO `products` (`id`, `product-name`, `price`, `stock-quantity`, `sku`)
+VALUES
+	(1,'Nuclear Warhead',1000000000.00,50,'NUC-1'),
+	(2,'Druggy Bag Surprise',1000.00,500,'DRU-2'),
+	(3,'Liver - Human',550000.00,15,'LIV-3'),
+	(4,'Siberian Tiger',5000000.00,2,'TIG-4'),
+	(5,'Thermite - Bucket',20000000.00,3,'THE-5'),
+	(6,'Coronavirus - Yoghurt Pot',10.00,10000,'COR-6');
+
+/*!40000 ALTER TABLE `products` ENABLE KEYS */;
+UNLOCK TABLES;
+
+
+
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
