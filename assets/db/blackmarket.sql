64d7a5925059ca30f64b6cc00070caeffef22cb9
HannaSoderlund <hanna.ej.soderlund@gmail.com>
2021-12-16 10:15:08 +0000

    added all tables to db

--- assets/db/blackmarket.sql
+++ assets/db/blackmarket.sql
@@ -7,11 +7,7 @@
 #
 # Host: 127.0.0.1 (MySQL 5.7.35)
 # Database: blackmarket
-<<<<<<< HEAD:assets/db/blackMarket.sql
-# Generation Time: 2021-12-16 10:07:36 +0000
-=======
-# Generation Time: 2021-12-16 09:32:54 +0000
->>>>>>> b92034aa3d67878b04ccc89e7db45a54bcdfb499:assets/db/blackmarket.sql
+# Generation Time: 2021-12-16 10:14:09 +0000
 # ************************************************************
 
 
@@ -24,7 +20,22 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 
-<<<<<<< HEAD:assets/db/blackMarket.sql
+# Dump of table customers
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `customers`;
+
+CREATE TABLE `customers` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `customer-address` varchar(255) NOT NULL DEFAULT '',
+  `customer-postcode` varchar(10) NOT NULL DEFAULT '',
+  `customer-email` varchar(255) NOT NULL DEFAULT '',
+  `customer-id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+
 # Dump of table orders
 # ------------------------------------------------------------
 
@@ -36,21 +47,41 @@ CREATE TABLE `orders` (
   `product-sku` varchar(30) NOT NULL DEFAULT '',
   `product-quantity` smallint(5) unsigned NOT NULL DEFAULT '0',
   `customer-id` int(11) NOT NULL,
-=======
-# Dump of table customers
-# ------------------------------------------------------------
-
-DROP TABLE IF EXISTS `customers`;
-
-CREATE TABLE `customers` (
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `customer-address` varchar(255) NOT NULL DEFAULT '',
-  `customer-postcode` varchar(10) NOT NULL DEFAULT '',
-  `customer-email` varchar(255) NOT NULL DEFAULT '',
->>>>>>> b92034aa3d67878b04ccc89e7db45a54bcdfb499:assets/db/blackmarket.sql
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+
+
+# Dump of table products
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `products`;
+
+CREATE TABLE `products` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `product-name` varchar(50) NOT NULL DEFAULT '',
+  `price` decimal(13,2) unsigned NOT NULL DEFAULT '0.00',
+  `stock-quantity` smallint(5) unsigned NOT NULL DEFAULT '0',
+  `sku` varchar(30) NOT NULL DEFAULT '',
+  `deleted` tinyint(1) DEFAULT '0',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `sku` (`sku`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+LOCK TABLES `products` WRITE;
+/*!40000 ALTER TABLE `products` DISABLE KEYS */;
+
+INSERT INTO `products` (`id`, `product-name`, `price`, `stock-quantity`, `sku`, `deleted`)
+VALUES
+	(9,'Nuclear Warhead',1000000000.00,50,'NUC-BATS91RD6KX7REHS5',0),
+	(10,'Druggy Bag Surprise',1000.00,500,'DRU-BATS91RD6KX7RFFJF',0),
+	(11,'Liver - Human',550000.00,15,'LIV-BATS91RD6KX7RFNI3',0),
+	(12,'Siberian Tiger',5000000.00,50,'SIB-BATS91RD6KX7RFUV1',0),
+	(13,'Thermite - Bucket',20000000.00,3,'THE-BATS91RD6KX7RG2J3',0),
+	(14,'Coronavirus - Yoghurt Pot',10.00,10000,'COR-BATS91RD6KX7RG8ZC',0);
+
+/*!40000 ALTER TABLE `products` ENABLE KEYS */;
+UNLOCK TABLES;
 
 
 
